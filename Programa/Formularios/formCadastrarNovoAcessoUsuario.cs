4d11f1fe39cb6ffec35e8680bdb3fed44533226b
Kaio Alves <Kaio.alves06@hotmail.com>
2024-07-18 23:12:35 -0300

    Ajustes

--- Programa/Formularios/formCadastrarNovoAcessoUsuario.cs
+++ Programa/Formularios/formCadastrarNovoAcessoUsuario.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -34,10 +35,35 @@ namespace Programa.Formularios
 
         private void btnCadastrarUser_Click(object sender, EventArgs e)
         {
-            CadastrarAcessoUsuario cadastrarUser = new CadastrarAcessoUsuario(this);
-            cadastrarUser.CadastroUsuario();
+            if (txtNomeUser.Text.Trim() == "")
+            {
+                MessageBox.Show("Informe o nome do Usuário", "ERRO", MessageBoxButtons.OK);
+                return;
+            }
 
-            MessageBox.Show("Usuário cadastrado com sucesso!", "CADASTRADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (txtSenhaUser.Text.Trim() == "")
+            {
+                MessageBox.Show("Informe a senha do Usuário", "ERRO", MessageBoxButtons.OK);
+                return ;
+            }
+            if (txtSenhaUser2.Text.Trim() == "")
+            {
+                MessageBox.Show("Confirme a senha", "ERRO", MessageBoxButtons.OK);
+                return;
+            }
+            if (txtSenhaUser.Text.Trim() != txtSenhaUser2.Text)
+            {
+                MessageBox.Show("As senhas estão diferentes, favor revisar", "ERRO", MessageBoxButtons.OK);
+                return;
+            }
+                           
+            
+                CadastrarAcessoUsuario cadastrarUser = new CadastrarAcessoUsuario(this);
+                cadastrarUser.CadastroUsuario();
+
+                MessageBox.Show("Usuário cadastrado com sucesso!", "CADASTRADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+            
         }
     }
 }
